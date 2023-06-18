0851bd613eb799329642beb240a4454d31fa7c76
Gustavo Morini <gustavommh@gmail.com>
2023-06-18 09:17:46 -0300

    options display admin area

--- contact/admin.py
+++ contact/admin.py
@@ -4,4 +4,10 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id', 'first_name', 'last_name', 'phone'
+    ordering = 'id',
+    list_filter = 'created_at',
+    search_fields = 'id', 'first_name', 'last_name'
+    list_per_page = 10
+    list_max_show_all = 30
+    list_display_links = 'id', 'first_name',
