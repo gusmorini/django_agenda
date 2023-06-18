78f8cd25a89cea33d8ab2ac42b585938c8f3e006
Gustavo Morini <gustavommh@gmail.com>
2023-06-18 10:18:14 -0300

    config media folders

--- core/settings.py
+++ core/settings.py
@@ -120,10 +120,19 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# send files
+MEDIA_URL = 'media/'
+
 # custom static files
 STATICFILES_DIRS = [
     BASE_DIR / 'base_static'
 ]
+
+# collectstatic
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
