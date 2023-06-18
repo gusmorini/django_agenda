e758b8725fb12714584aa0c559e5a32e1203932b
Gustavo Morini <gustavommh@gmail.com>
2023-06-18 10:43:59 -0300

    model category

--- contact/admin.py
+++ contact/admin.py
@@ -11,3 +11,8 @@ class ContactAdmin(admin.ModelAdmin):
     list_per_page = 10
     list_max_show_all = 30
     list_display_links = 'id', 'first_name',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = 'id',
