b591f71af45ea7f73f9537eaaa9045c7e63731e8
Gustavo Morini <gustavommh@gmail.com>
2023-06-22 09:04:22 -0300

    title pages

--- contact/views/index.py
+++ contact/views/index.py
@@ -4,11 +4,12 @@ from contact.models import Contact
 
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('id')[0:10]
-    context = {'contacts': contacts}
+    context = {'contacts': contacts, 'title': 'Contatos'}
     return render(request, 'contact/index.html', context,)
 
 
 def contact(request, contact_id):
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
-    context = {'contact': single_contact, }
+    context = {'contact': single_contact,
+               'title': f'{single_contact.first_name} {single_contact.last_name}'}
     return render(request, 'contact/contact.html', context,)
