a3954691612550eaae1d83144384cd072af59df6
Gustavo Morini <gustavommh@gmail.com>
2023-06-23 16:56:14 -0300

    fix url search pagination

--- contact/views/index.py
+++ contact/views/index.py
@@ -7,7 +7,7 @@ from django.core.paginator import Paginator
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('id')
 
-    paginator = Paginator(contacts, 20)
+    paginator = Paginator(contacts, 10)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
@@ -34,10 +34,10 @@ def search(request):
         show=True
     ).order_by('id')
 
-    paginator = Paginator(contacts, 20)
+    paginator = Paginator(contacts, 10)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
     context = {'page_obj': page_obj, 'title': 'Contatos',
-               'search_value': search_value}
+               'search_value': search_value, 'url_search': f'&q={search_value}'}
     return render(request, 'contact/index.html', context)
