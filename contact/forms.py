f7f91cc530642810521a60028cf0ee6ec9ed435b
Gustavo Morini <gustavommh@gmail.com>
2023-07-01 15:03:38 -0300

    create contact

--- contact/forms.py
+++ contact/forms.py
@@ -60,19 +60,19 @@ class ContactForm(forms.ModelForm):
     def clean(self):
         # cleaned_data = self.cleaned_data
 
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
-        )
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
+        # self.add_error(
+        #     'first_name',
+        #     ValidationError(
+        #         'Mensagem de erro',
+        #         code='invalid'
+        #     )
+        # )
+        # self.add_error(
+        #     'first_name',
+        #     ValidationError(
+        #         'Mensagem de erro 2',
+        #         code='invalid'
+        #     )
+        # )
 
         return super().clean()
--- contact/views/index.py
+++ contact/views/index.py
@@ -5,7 +5,7 @@ from django.core.paginator import Paginator
 
 
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('id')
+    contacts = Contact.objects.filter(show=True).order_by('-id')
 
     paginator = Paginator(contacts, 10)
     page_number = request.GET.get("page")
